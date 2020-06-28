21bd7a425a9b5de1986c557ed83ac863d9b03154
NguyennCuong <cuong14863@gmail.com>
2020-06-28 11:45:02 +0700

    update giao dien: sua loai benh

--- resources/views/index/loaibenh/sua.blade.php
+++ resources/views/index/loaibenh/sua.blade.php
@@ -1,66 +1,72 @@
 @extends('index.layout.index')
 @section('title')
-    <title>Sửa loại bệnh {{$LoaiBenh->TenLoaiBenh}} - Quản lý phòng mạch tư</title>
+<title>Sửa loại bệnh {{$LoaiBenh->TenLoaiBenh}} - Quản lý phòng mạch tư</title>
 @endsection
 @section('style')
 @endsection
 @section('content')
-    <div class="row">
-        <div class="col-sm-12">
-            <ol class="breadcrumb">
-                <li>
-                    <a href=""><i class="ti-home"></i></a>
-                </li>
-                <li>
-                    <a href="{{route('ds-loaibenh.get')}}">Danh sách loại bệnh</a>
-                </li>
-                <li class="active">
-                    Sửa loại bệnh {{$LoaiBenh->TenLoaiBenh}}
-                </li>
-            </ol>
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-flex align-items-center justify-content-between">
+            <h4 class="mb-0 font-size-18">Sửa loại bệnh {{$LoaiBenh->TenLoaiBenh}}</h4>
+
+            <div class="page-title-right d-none d-lg-block">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="/">Trang chủ</a></li>
+                    <li class="breadcrumb-item active">Danh mục</li>
+                    <li class="breadcrumb-item active">Danh sách loại bệnh</li>
+                    <li class="breadcrumb-item active">Sửa loại bệnh</li>
+                </ol>
+            </div>
+
         </div>
     </div>
+</div>
 
-    @if (count($errors) > 0 || session('error'))
-        <div class="alert alert-danger" role="alert">
-            <strong>Cảnh báo!</strong><br>
-            @foreach($errors->all() as $err)
-                {{$err}}<br/>
-            @endforeach
-            {{session('error')}}
-        </div>
-    @endif
-    @if (session('success'))
-        <div class="alert alert-success">
-            <strong>Thành công!</strong>
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <br/>
-            {{session('success')}}
-        </div>
-    @endif
+@if (count($errors) > 0 || session('error'))
+<div class="alert alert-danger" role="alert">
+    <strong>Cảnh báo!</strong><br>
+    @foreach($errors->all() as $err)
+    {{$err}}<br />
+    @endforeach
+    {{session('error')}}
+</div>
+@endif
+@if (session('success'))
+<div class="alert alert-success">
+    <strong>Thành công!</strong>
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <br />
+    {{session('success')}}
+</div>
+@endif
 
-    <!--end duong dan nho-->
-    <div class="row">
-        <div class="col-sm-6">
-            <div class="card-box">
-                <h4 class="m-t-0 header-title"><b>Sửa loại bệnh {{$LoaiBenh->TenLoaiBenh}}</b></h4>
+<!--end duong dan nho-->
+<div class="row">
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title mb-4">Sửa loại bệnh {{$LoaiBenh->TenLoaiBenh}}</h4>
                 <p class="text-muted m-b-10 font-13">
-                    <b>Bắt buộc</b> <code>Loại bệnh</code>
+                    <b>Bắt buộc</b>
+                    <span class="badge badge-pill badge-soft-primary font-size-11">Loại bệnh</span>
                 </p>
                 <div class="row">
                     <div class="col-md-12">
                         <div class="p-l-r-10">
-                            <form class="form-horizontal" role="form" action="{{route('sua-loaibenh.post',[$LoaiBenh->MaLoaiBenh])}}"
-                                  method="post">
+                            <form class="form-horizontal" role="form"
+                                action="{{route('sua-loaibenh.post',[$LoaiBenh->MaLoaiBenh])}}" method="post">
                                 {{csrf_field()}}
 
                                 <div class="form-group">
                                     <label class="control-label">Tên loại bệnh</label>
-                                    <input name="tenloaibenh" type="text" class="form-control" value="{{old('tenloaibenh',$LoaiBenh->TenLoaiBenh)}}" placeholder="Nhập tên loại bệnh...">
+                                    <input name="tenloaibenh" type="text" class="form-control"
+                                        value="{{old('tenloaibenh',$LoaiBenh->TenLoaiBenh)}}"
+                                        placeholder="Nhập tên loại bệnh...">
                                 </div>
 
                                 <div class="form-group">
-                                    <button class="ladda-button btn btn-default" data-style="expand-right">Lưu lại
+                                <button class="btn btn-primary" data-style="expand-right">Lưu lại
                                     </button>
                                 </div>
                             </form>
@@ -70,6 +76,7 @@
             </div>
         </div>
     </div>
+</div>
 
 @endsection
 @section('script-ori')
